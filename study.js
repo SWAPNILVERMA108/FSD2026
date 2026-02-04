7444dc28b56de708f67b10c55f2ddf0cdd7b1d36
SWAPNIL VERMA <swapnilverma109@gmail.com>
2026-02-04 15:53:29 +0530

    js

--- study.js
+++ study.js
@@ -1,3 +1,57 @@
-function changeText() {
-    document.getElementById("text").innerHTML = "You clicked the button! 🎉";
-}
+// var a =10;
+// let b = 20;
+// const c = 30;
+
+
+// let bool = true;
+
+// let nul = null;
+
+// console.log(a+b);
+// console.log(typeof(a));
+// console.log(typeof(b));
+// console.log(typeof(c));
+
+
+
+// // array 
+
+// let arr = [10,20,30,40];
+// console.log("value of array at index 2 is :" ,arr[2])
+
+
+// let student ={
+//     name : "vikas",
+//     age:27
+// }
+
+// // function 
+
+// function add(p,q){
+//     return p+q;
+// }
+// console.log(add(5,7));
+
+
+// // arrow function 
+
+// const multiply= (p,q) =>p*q;
+// console.log("arrow function : ", multiply(5,10));
+
+// // date 
+// let today = new Date();
+// console.log(today)
+
+
+console.log(Math.sqrt(25))
+let a = ((Math.random()+1));
+
+// string
+txt = "hello world";
+console.log(txt.includes("html"));
+
+// type conversion
+
+let strnum= "123";
+let convert = Number(strnum);
+console.log(convert+10)
