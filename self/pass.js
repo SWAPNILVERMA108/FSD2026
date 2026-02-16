7bd94d7c25076157bb2c029388cb117f2c79a4bc
SWAPNIL VERMA <swapnilverma109@gmail.com>
2026-02-16 21:37:17 +0530

    password generator

---
+++ self/pass.js
@@ -0,0 +1,16 @@
+function generatePassword() {
+    const length = document.getElementById("length").value;
+    const output = document.getElementById("password");
+
+    const characters =
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    let password = "";
+
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * characters.length);
+        password += characters[randomIndex];
+    }
+
+    output.value = password;
+}
