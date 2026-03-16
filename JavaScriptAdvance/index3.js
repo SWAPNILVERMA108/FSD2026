d22d1ae685a78af588dbca5ea57bf02076d944c6
SWAPNIL VERMA <swapnilverma109@gmail.com>
2026-03-16 10:08:07 +0530

    deep copy

--- JavaScriptAdvance/index3.js
+++ JavaScriptAdvance/index3.js
@@ -1,7 +1,23 @@
-let arr = [1, 2, 3, 4, 5];
-let arrcopy = {...arr};
-arrcopy[0] = 10;
-console.log(arr);
-console.log(arrcopy);
+// let arr = [1, 2, 3, 4, 5];
+// let arrcopy = {...arr};
+// arrcopy[0] = 10;
+// console.log(arr);
+// console.log(arrcopy);
 
+
+
+// deep copy
+
+let user ={
+    name : "swapnil",
+    age :27,
+    address:{
+        city:"delhi",
+        pincode:110001
+    }
+};
+ let deepcopy = JSON.parse(JSON.stringify(user));
+ deepcopy.address.city = "noida";
+ console.log(user);
+ console.log(deepcopy);
 
